530e9b50f8955b8b2e8fc95601f755a0885eabaf
varkatamyagnavallika <141001313+varkatamyagnavallika@users.noreply.github.com>
2024-12-20 08:50:10 +0000

    Basics-variable,datatype

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //It is used to treat all the Javascript code as newer version 
+let studentName="Yagnavallika";
+let studentRollno=19;
+let studentBranch="CSE";
+let presentToCollege=true;
+let collegeName;
+let jobOffers=null;
+
+//To declare the data type of the variable.
+console.log();
+console.log("Declaring the dataype with the use of variable :- ");
+console.log();
+console.log(typeof studentName);
+console.log(typeof studentRollno);
+console.log(typeof studentBranch);
+console.log(typeof presentToCollege);
+console.log(typeof collegeName);
+console.log(typeof jobOffers);
+console.log();
+
+
+// other way of knowing the datatype with out the variable declaration.
+console.log("Declaring the datatype without a variable declaration. ");
+console.log();
+console.log(typeof "Yagnavallika");
+console.log(typeof 19);
+console.log(typeof true);
+console.log(typeof null);  //null is an object.
