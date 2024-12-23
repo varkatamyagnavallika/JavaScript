7970d11ffcbb2073afe85c889710933c4f630366
varkatamyagnavallika <141001313+varkatamyagnavallika@users.noreply.github.com>
2024-12-23 05:19:20 +0000

    Basics - Variable, Datatype,Datatype Conversations

---
+++ 01_Basics/04_Datatype_Conversations_type.js
@@ -0,0 +1,15 @@
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true...
+
+/* The reason why the 1st two conditions has given it as 
+false and 3rd condition has given the output as false, Because 
+The equaly to operator (==) and comparisons >, <,  >=, 
+<= works differently.
+Comparisons convert null to a number, treating it as 0.
+In 1st statement we can see that there is a comparision operator but the symbol
+greator than . as comparision operator converts the null to
+0(Zero) so we got an output as false.
+Thats why the 3rd conditions gives output as true
+and remaining as false.*/
+
