d401f3ce990f141c1880919ea0b4c7ecf84add16
varkatamyagnavallika <141001313+varkatamyagnavallika@users.noreply.github.com>
2024-12-23 06:27:51 +0000

    Heap

---
+++ 02_Stack_and_Heap/02_Heap.js
@@ -0,0 +1,22 @@
+// Non-Primitive datatypes will be stored in heap memory in the form of references.
+
+let studentOne={
+    studentName: "Yagnavallika",
+    studentEmail : "Yagnavallika@gmail.com",
+    studentRollno : 19,
+}
+
+let studentTwo= studentOne;
+console.log(studentOne.studentName); //Yagnavallika
+console.log(studentTwo.studentName); //Yagnavallika
+
+studentTwo.studentName="Radha";
+console.log(studentOne.studentName); //Radha
+console.log(studentTwo.studentName); //Radha
+
+/* In stack when we try to change one variable data only that data has been changed because when we use refer
+it will work on the copy one not on the original variable.
+but here in heap memory when we try to change on variable data it has been changed for two vaiables
+beacause in heap the both variables have been refered so there wont be a copy as in stack
+it will be directly refered to the original variable so any change done to any one variable then 
+the refered one will also be changed. */
