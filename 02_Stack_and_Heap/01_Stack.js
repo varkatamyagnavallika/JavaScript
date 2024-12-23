3df1ea5abc0e62790d1bb0dc5e862bd94ed5fb36
varkatamyagnavallika <141001313+varkatamyagnavallika@users.noreply.github.com>
2024-12-23 06:16:11 +0000

    Stack and Heap

---
+++ 02_Stack_and_Heap/01_Stack.js
@@ -0,0 +1,15 @@
+//Primitive datatypes are stored in stack memory.
+ 
+let myName="Yagnavallika"
+let anotherName=myName
+
+console.log(myName) //Yagnavallika
+console.log(anotherName) //Yagnavallika
+//If we want to change the value of another name
+
+anotherName="Varkatam"
+
+console.log(myName) //Yagnavallika
+console.log(anotherName) //Varkatam
+
+/* Here in stack when we try to change the value of anotherName only that value will be changed. */
