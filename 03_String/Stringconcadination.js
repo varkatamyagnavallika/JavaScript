707f25bccd4dbf642c14467639c3049312cc3429
varkatamyagnavallika <141001313+varkatamyagnavallika@users.noreply.github.com>
2024-12-24 05:21:11 +0000

    String Concadination

---
+++ 03_String/Stringconcadination.js
@@ -0,0 +1,9 @@
+const name = "Yagnavallika"
+const emailId="yagnavallika@gmail.com"
+const id=19
+
+// Conacadination using + 
+console.log(name+emailId+id); //Yagnavallikayagnavallika@gmail.com19
+
+//Concadination using back ticks
+console.log(`The name of the student is ${name} and her roll id is ${id} and her emailid is ${emailId}`);
