0b31a08050f593e31612ee7869ca55912e23e6d6
varkatamyagnavallika <141001313+varkatamyagnavallika@users.noreply.github.com>
2024-12-28 09:24:04 +0000

    Object - JSON

--- 06_Objects/Objects.js
+++ 06_Objects/Objects.js
@@ -27,3 +27,82 @@ jsuser.greetingTwo=function() {
 }
 console.log(jsuser.greeting);  // function return back it has not exectuted but returned
 console.log(jsuser.greetingTwo());
+
+
+
+//const tinderUser=new  Object(); --> Singleton object
+const tinderUser={} //Non singleton object
+tinderUser.id="123abcd";
+tinderUser.name="Yagna";
+tinderUser.isLoggedIn=false;
+
+console.log(tinderUser);
+
+const regularUser={
+    email: "yagna@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Varkatam",
+            lastname: "Yagnavallika"
+        }
+    }
+}
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a", 2: "b", 3: "c"}
+const obj2={4:"a", 5: "b", 6: "c"}
+// const obj3={obj1,obj2}
+//or
+//const obj3=Object.assign({},obj1,obj2); //--> here {} is target and obj1,obj2 is source.
+//or
+const obj3={...obj1,...obj2};
+console.log(obj3);
+
+//When you get values from database
+const users=[
+    {
+        id:1,
+        email:"V@gmail.com"
+    },
+    {   
+    },
+    {
+    },
+]
+users[1].email;
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+//API'S
+//Destructuring
+const course={
+    courseName:"JavaScript",
+    CouseInstructor:"Yagnavallika"
+}
+// course.courseName
+const {courseName}=course
+//Or like const {courseName : Name}=course
+console.log(courseName); //console.log(Name);
+ 
+//Destructuring in react
+// const navbar=({company}) => {
+
+// }
+// navbar(company="Yagnavallika")
+
+//API'S --> JSON (object)-->Representation {}
+// {
+//     "name":"Yagnavallika",
+//     "coursername":"Js"
+// }
+//In the form of arrays
+// [
+//     {},
+//     {},
+//     {}
+// ] //Random user me api -->Most useful api
